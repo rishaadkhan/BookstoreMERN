251a947ec8ffd2426b219555c2c4657a2879068b
Rishaad <rishaad326@gmail.com>
2024-07-04 19:46:55 +0530

    Final Project

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://rishaad326:rishaad326@bookstore.nd804fw.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
